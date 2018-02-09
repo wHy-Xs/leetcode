e32df174fe44befbc28088720f7e105a804ca86b
wHy-Xs <603987690@qq.com>
2018-02-09 18:19:00 +0800

    Create 回文字符串相关.java

---
+++ 回文字符串相关.java
@@ -0,0 +1,33 @@
+//1.判断一个字符串是否是回文字符串
+ 
+ public void panduan(){
+  for (int i = 0; i < str.length() / 2; i++) {  
+          if ((str.substring(i, i + 1)).equals(str.substring(str.length() - 1- i, str.length() - i))) {  
+                  count++;  
+              }  
+          }  
+          if (count == str.length() / 2) {  
+              System.out.println("是回文字符串");  
+          }else{  
+              System.out.println("不是回文字符串");  
+          }  
+    }
+    
+public void panduan1(){
+    StringBuffer sb = new StringBuffer(str);  
+    sb.reverse();// 将Str中的字符串倒置  
+  
+    int count = 0;  
+    for (int i = 0; i < str.length(); i++) {  
+        if (str.charAt(i) == sb.charAt(i)) {  
+            count++;  
+        }  
+    }  
+    if (count == str.length()) {  
+          
+        System.out.println("此字符串是一个回文字符串");  
+    } else {  
+        System.out.println("此字符串不是一个回文字符串");  
+    }  
+}
+//2.找出一个字符串中最长的回文字符串
